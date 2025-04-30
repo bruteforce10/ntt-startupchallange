2bee97596109a5f639547097ee00b374542712eb
bruteforce10 <audifirdi@gmail.com>
2025-05-01 00:15:13 +0700

    refactor(data-startup): convert data fetching to use SWR for improved loading state management
    - Replaced the async data fetching function with SWR for better data handling and loading state.
    - Added loading indicator while data is being fetched from the API.
    - Updated the component structure to support the new data fetching method.

--- src/app/data-startup/page.jsx
+++ src/app/data-startup/page.jsx
@@ -1,26 +1,27 @@
+"use client";
+
 import { columns } from "@/components/data-table/columns";
 import { DataTable } from "@/components/data-table/data-table";
 import InitialPage from "@/components/pages/initial-page";
+import fetcher from "@/utils/fetcher";
 import React from "react";
+import useSWR from "swr";
 
-async function getData() {
-  const res = await fetch("/api/get-data-startup", {
-    cache: "no-store",
-    next: { revalidate: 0 },
-  });
-  const data = await res.json();
-
-  return data;
-}
-
-export default async function DataStartupPage() {
-  const data = await getData();
+const DataStartupPage = () => {
+  const { data, error, isLoading } = useSWR("/api/get-data-startup", fetcher);
+  console.log(data);
 
   return (
     <InitialPage>
       <main className="container mx-auto px-4 py-8">
-        <DataTable columns={columns} data={data} />
+        {isLoading ? (
+          <div>Loading...</div>
+        ) : (
+          <DataTable columns={columns} data={data} />
+        )}
       </main>
     </InitialPage>
   );
-}
+};
+
+export default DataStartupPage;
