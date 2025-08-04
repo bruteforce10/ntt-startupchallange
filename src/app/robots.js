50b002168381c6ce422452a50257a1bf1758a2a6
bruteforce10 <audifirdi@gmail.com>
2025-08-04 17:06:26 +0700

    add robot

--- src/app/robots.js
+++ src/app/robots.js
@@ -3,7 +3,6 @@ export default function robots() {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: "/private/",
     },
     sitemap: "https://www.ntt-startupchallenge.com/sitemap.xml",
   };
