d6c3f33835ce8a8836db51c8cc5f943da8ae2db0
bruteforce10 <audifirdi@gmail.com>
2025-08-22 11:26:57 +0700

    feat: refactor CtaHero component to remove DropdownMenu and enhance button functionality

--- src/components/modules/cta-hero.jsx
+++ src/components/modules/cta-hero.jsx
@@ -13,7 +13,16 @@ import {
 const CtaHero = () => {
   return (
     <div className="flex gap-4">
-      <DropdownMenu>
+      <Button className="bg-blue-ntt border-none rounded-md text-white px-6 py-5 cursor-pointer hover:bg-blue-ntt/80">
+        <Link
+          href={"https://www.eventbrite.com/e/1323350509999?aff=oddtdtcreator"}
+          className="block"
+          target="_blank"
+        >
+          Attend Final Day
+        </Link>
+      </Button>
+      {/* <DropdownMenu>
         <DropdownMenuTrigger className="bg-blue-ntt border-none rounded-md text-white px-6 py-2 cursor-pointer hover:bg-blue-ntt/80">
           Register
         </DropdownMenuTrigger>
@@ -39,7 +48,7 @@ const CtaHero = () => {
             </Link>
           </DropdownMenuItem>
         </DropdownMenuContent>
-      </DropdownMenu>
+      </DropdownMenu> */}
     </div>
   );
 };
