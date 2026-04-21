f74ba8743b2e28507ad73092a4da942ffe6484b4
bruteforce10 <audifirdi@gmail.com>
2026-04-21 10:03:47 +0700

    refactor: comment out unused startup tab and content in RegisterTabs component

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -27,12 +27,12 @@ function RegisterTabs() {
       className="w-full"
     >
       <TabsList className="grid grid-cols-2 bg-transparent w-full mb-8">
-        <TabsTrigger
+        {/* <TabsTrigger
           value="startup"
           className=" data-[state=active]:bg-transparent data-[state=active]:shadow-none data-[state=active]:text-white data-[state=active]:border-b-2 data-[state=active]:border-blue-500 rounded-lg text-gray-400"
         >
           Startup
-        </TabsTrigger>
+        </TabsTrigger> */}
         <TabsTrigger
           value="partner"
           className="w-full data-[state=active]:bg-transparent data-[state=active]:shadow-none data-[state=active]:text-white data-[state=active]:border-b-2 data-[state=active]:border-blue-500 rounded-lg text-gray-400"
@@ -41,9 +41,9 @@ function RegisterTabs() {
         </TabsTrigger>
       </TabsList>
 
-      <TabsContent value="startup">
+      {/* <TabsContent value="startup">
         <FormStartup />
-      </TabsContent>
+      </TabsContent> */}
 
       {/* Partner Registration Form */}
       <TabsContent value="partner">
